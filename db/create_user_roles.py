5c9536fef96f6d9322fe9b7214a9fa56050793e7
mikal <s1135287@student.hsleiden.nl>
2023-12-01 19:03:32 +0100

    Fix: tests use sqlite db instead of mysql db

--- db/create_user_roles.py
+++ db/create_user_roles.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 
 import mysql.connector
 from dotenv import load_dotenv
@@ -7,15 +8,29 @@ load_dotenv()
 
 
 def create_roles():
-    conn = mysql.connector.connect(
-        host=os.getenv('DB_HOST'),
-        user=os.getenv('DB_USERNAME'),
-        password=os.getenv('DB_PASSWORD'),
-        database=os.getenv('DB_NAME'),
-        port=os.getenv('DB_PORT')
-    )
+    testing = os.getenv("FLASK_TEST_ENV") == "test"
+    if testing:
+        conn = sqlite3.connect(':memory:')
+    else:
+        conn = mysql.connector.connect(
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USERNAME'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_NAME'),
+            port=os.getenv('DB_PORT')
+        )
 
     cursor = conn.cursor()
+
+    if testing:
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='role';")
+        table_exists = cursor.fetchone()
+
+        if not table_exists:
+            cursor.execute(
+                "CREATE TABLE role (id INTEGER PRIMARY KEY, name VARCHAR(80) UNIQUE);"
+            )
+
     cursor.execute("SELECT id FROM role WHERE name='user' OR name='admin';")
     existing_roles = cursor.fetchall()
 
