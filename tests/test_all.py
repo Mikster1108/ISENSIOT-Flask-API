5adc29410997ff73da30a42715ed3a09bd3dbe38
mikal <s1135287@student.hsleiden.nl>
2023-12-01 19:04:21 +0100

    Added basic tests for authentication

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,21 +2,29 @@ import http.client
 import json
 import os
 import unittest
+
+from dotenv import load_dotenv
 from faker import Faker
 from werkzeug.security import generate_password_hash
+
+from db.create_user_roles import create_roles
 
 os.environ["FLASK_TEST_ENV"] = "test"   # Variable needs to be set before importing app to use test database
 
 from app import app
-from app_setup import user_datastore, db
+from app_setup import user_datastore, db, security
 
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
         self.user_datastore = user_datastore
+        self.security = security
         self.faker = Faker()
         self.headers = {'Content-Type': 'application/json'}
+
+        create_roles()
+        load_dotenv()
 
     def get_random_user(self, password, email=None):
         with app.app_context():
@@ -48,7 +56,9 @@ class BaseTestCase(unittest.TestCase):
 class TestAuthentication(BaseTestCase):
     def setUp(self):
         super().setUp()
+
         self.base_path = "/user"
+        self.access_code = os.getenv("ACCESS_CODE")
 
     def test_should_return_status_200_when_logging_in_using_valid_credentials(self):
         password = "valid_credentials"
@@ -56,24 +66,53 @@ class TestAuthentication(BaseTestCase):
         user = self.get_random_user(password=password)
         data = json.dumps({"email": user.email, "password": password})
 
-        response = self.client.post(f"/user/login", data=data, headers=self.headers)
+        response = self.client.post(f"{self.base_path}/login", data=data, headers=self.headers)
 
         assert response.status_code == http.client.OK
 
     def test_should_return_status_400_when_logging_in_using_invalid_credentials(self):
-        pass
+        false_password = "invalid_credentials"
+        actual_password = "random password"
+
+        user = self.get_random_user(password=actual_password)
+        bad_password_data = json.dumps({"email": user.email, "password": false_password})
+        bad_email_data = json.dumps({"email": self.faker.email(), "password": actual_password})
+
+        bad_password_response = self.client.post(f"{self.base_path}/login", data=bad_password_data, headers=self.headers)
+        bad_email_data = self.client.post(f"{self.base_path}/login", data=bad_email_data, headers=self.headers)
+
+        assert bad_password_response.status_code == http.client.UNAUTHORIZED
+        assert bad_email_data.status_code == http.client.BAD_REQUEST
 
     def test_should_return_status_201_when_registering_using_valid_details(self):
-        pass
+        user_data = json.dumps({
+            "email": self.faker.email(),
+            "password": "henk03",
+            "access_code": self.access_code})
+
+        response = self.client.post(f"{self.base_path}/register", data=user_data, headers=self.headers)
+
+        assert response.status_code == http.client.CREATED
 
     def test_should_return_status_400_when_registering_using_wrong_access_code(self):
-        pass
+        user_data = json.dumps({
+            "email": self.faker.email(),
+            "password": "henk03",
+            "access_code": "Wrong_code"})
+
+        response = self.client.post(f"{self.base_path}/register", data=user_data, headers=self.headers)
+        assert response.status_code == http.client.BAD_REQUEST
 
     def test_should_return_status_401_when_requesting_sensor_data_while_not_logged_in(self):
-        pass
+        response = self.client.get(f"/sensor_data", follow_redirects=True, headers=self.headers)
+        assert response.status_code == http.client.UNAUTHORIZED
 
     def test_should_return_status_401_when_requesting_sensor_data_using_false_token(self):
-        pass
+        headers = self.headers
+        fake_token = security.remember_token_serializer.dumps("df55a3a4a2e3481a8c2b37ab70ac3736")
+        headers.update({'Authorization': f'Bearer {fake_token}'})
+        response = self.client.get(f"/sensor_data", follow_redirects=True, headers=headers)
+        assert response.status_code == http.client.UNAUTHORIZED
 
 
 class TestSensorData(BaseTestCase):
