3434de10e294a9ef73dd3ad30bc7eb12c392b402
BoopidyBappidy <83213010+IonMiddelraad@users.noreply.github.com>
2023-12-15 12:35:56 +0100

    any added file in the directory gets analyzed

--- machine_learning/object_recognition/detector_controller.py
+++ machine_learning/object_recognition/detector_controller.py
@@ -1,13 +1,17 @@
-from detector import *
 import os
+
+from machine_learning.object_recognition.detector import Detector
 
 
 class DetectorController:
     def analyze_video(video_path=str):
-        configPath = os.path.join("model_data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
-        modelPath = os.path.join("model_data", "frozen_inference_graph.pb")
-        classesPath = os.path.join("model_data", "coco.names")
-        filterPath = os.path.join("model_data", "filter-object-list.names")
+        project_root = os.path.dirname(os.path.abspath(__file__))
+        data_folder = "model_data"
+
+        configPath = os.path.join(project_root, data_folder, "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
+        modelPath = os.path.join(project_root, data_folder, "frozen_inference_graph.pb")
+        classesPath = os.path.join(project_root, data_folder, "coco.names")
+        filterPath = os.path.join(project_root, data_folder, "filter-object-list.names")
 
         object_detector = Detector(video_path, configPath, modelPath, classesPath, filterPath)
         itemsFoundList, activationItem = object_detector.onVideo()
