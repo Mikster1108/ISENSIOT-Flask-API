08b5d453a517e5a5e107caf54802f6f8c5d9f1b5
mikal <s1135287@student.hsleiden.nl>
2023-12-18 16:32:55 +0100

    Fix: added extra checks for validating email and password

--- user/__init__.py
+++ user/__init__.py
@@ -1,5 +1,6 @@
 import http.client
 import os
+import sqlalchemy
 from flask import Blueprint, jsonify, request, abort
 from flask_login import login_user
 from app_setup import user_datastore, db, security, limiter
@@ -7,7 +8,7 @@ from security import SCFlask
 from user.fetch_users import fetch_all_users, fetch_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from user.user_roles import Rolename, all_roles
-
+from user.validate_parameters import validate_email, validate_password
 
 api = Blueprint('user', __name__)
 requires_authentication = SCFlask.requires_authentication
@@ -17,20 +18,28 @@ requires_authentication = SCFlask.requires_authentication
 @limiter.limit("5 per minute")
 def register():
     code = request.json.get('access_code')
+    email = request.json.get('email')
+    password = request.json.get('password')
+
     if not code:
         abort(http.client.BAD_REQUEST, "Access code is missing")
     else:
         if code != os.getenv('ACCESS_CODE'):
             abort(http.client.BAD_REQUEST, "Access code is incorrect")
 
+    validate_email(email)
+    validate_password(password)
+
     try:
         user = user_datastore.create_user(
-            email=request.json.get('email'),
-            password=generate_password_hash(request.json.get('password')),
+            email=email,
+            password=generate_password_hash(password),
         )
         db.session.commit()
+    except sqlalchemy.exc.IntegrityError as e:
+        abort(http.client.BAD_REQUEST, "Email already in use")
     except Exception as e:
-        abort(http.client.BAD_REQUEST, "Something went wrong with storing user to database, check mail and password")
+        abort(http.client.BAD_REQUEST, "Something went wrong with storing user to database")
 
     return jsonify(user.serialize()), 201
 
@@ -42,8 +51,6 @@ def login():
     password = request.json.get('password')
 
     user = fetch_user(email)
-    if not user:
-        abort(http.client.NOT_FOUND, "User not found")
 
     if check_password_hash(user.password, password):
         login_user(user)
