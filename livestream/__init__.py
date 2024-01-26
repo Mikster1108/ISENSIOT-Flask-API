1e2f57b8073356d8283994a3f20a791c6101d439
mikal <s1135287@student.hsleiden.nl>
2024-01-26 01:19:05 +0100

    Changed start recording endpoint to work with sockets instead

--- livestream/__init__.py
+++ livestream/__init__.py
@@ -1,14 +1,15 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from livestream.handle_camera_feed import CameraThread
 from livestream.record_stream import connect_ssh
 from livestream.socket_listeners import addListener, removeListener, getListenersAmount
 from security import SCFlask
-from app_setup import socketio, limiter
+from app_setup import socketio
 from flask_socketio import emit
 
 api = Blueprint('livestream', __name__)
 requires_authentication = SCFlask.requires_authentication
 camera_thread = CameraThread()
+RECORDING_TIME_SECONDS = 90
 
 
 @socketio.on('connect')
@@ -35,14 +36,21 @@ def handle_stream_request():
         emit('start_stream_response', {'data': 'Stream is already running'})
 
 
-@limiter.limit("10 per minute")
-@api.route('/start-recording', methods=['GET'])
-@requires_authentication
+@socketio.on('start-recording')
 def start_recording():
     recording = connect_ssh()
+    # recording = True
     if recording:
-        return jsonify({"success": "Recording..."}), 200
+        emit('start_recording_response', {'data': 'Camera is recording...'})
+
+        socketio.start_background_task(target=emit_recording_message)
     elif recording is False:
-        return jsonify({"error": "Failed to start recording"}), 500
-    else:
-        return jsonify({"status": "Waiting for camera response"}), 200
+        emit('start_recording_response', {'error': 'Failed to start recording'})
+
+
+def emit_recording_message():
+    for _ in range(5):
+        socketio.sleep(1)
+        socketio.emit('start_recording_response', {'data': 'Camera is recording...'})
+    socketio.sleep(2)
+    socketio.emit('start_recording_response', {'data': 'Camera is done recording'})
